2f3aae24d0c52b10a48f0892fcc564609c0278a3
Denys <glushychden.10@gmail.com>
2026-03-15 19:42:22 +0200

    Solve exercise.

--- dwdwd.py
+++ dwdwd.py
@@ -1,30 +1,19 @@
-# nums1 = [1,2,2,1]
-# nums2 = [2,2]
-nums1 = [4,9,5,1001]
-nums2 = [9,4,9,8,4,1001]
+nums = [-1,0,3,5,9,12]
+target = 9
 
 
 class Solution:
-    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        result_nums1 = []
-        result_nums2 = []
-
-        if 1 <= len(nums1) <= 1000 and 1 <= len(nums2) <= 1000:
-            for i in range(0, len(nums1)):
-                if 0 <= nums1[i] <= 1000:
-                    n = nums1[i]
-                    result_nums1.append(n)
-            for i in range(0, len(nums2)):
-                if 0 <= nums2[i] <= 1000:
-                    u = nums2[i]
-                    result_nums2.append(u)
-
-        nums1set = set(result_nums1)
-        nums2set = set(result_nums2)
-
-        return list(nums1set.intersection(nums2set))
+    def search(self, nums: list[int], target: int) -> int:
+        if 1 <= len(nums) <= 10**4:
+            for i in nums:
+                if (-10**4 >= i or i >= 10**4) or (-10**4 >= target or target >= 10**4):
+                    return -1
+                if i == target:
+                    return nums.index(i)
+                if target not in nums:
+                    return -1
 
 
 solution = Solution()
-print(solution.intersection(nums1, nums2))
+print(solution.search(nums, target))
 
