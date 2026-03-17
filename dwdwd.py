f5ba78b870c931477146fdc193b7528abce84dc9
Denys <glushychden.10@gmail.com>
2026-03-17 17:24:59 +0200

    Solve exercise.

--- dwdwd.py
+++ dwdwd.py
@@ -1,15 +1,12 @@
-nums = [2,2]
-target = 4
+nums = [3,0,1]
 
 
 class Solution:
-    def two_sum(self, nums: list[int], target: int) -> list[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if 2 <= len(nums) <= 10 ** 4 and -10 ** 9 <= nums[i] <= 10 ** 9 and -10 ** 9 <= target <= 10 ** 9:
-                    if nums[i] + nums[j] == target:
-                        return [i, j]
+    def missing_number(self, nums: list[int]) -> int:
+        solve_all_numbers = (len(nums) * (len(nums) + 1)) / 2
+        solve_nums_numbers = sum(nums)
+        return int(solve_all_numbers - solve_nums_numbers)
 
 
 solution = Solution()
-print(solution.two_sum(nums, target))
+print(solution.missing_number(nums))
