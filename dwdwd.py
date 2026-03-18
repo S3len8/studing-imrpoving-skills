b2186dd7293d36885be9b08b321b067fd657201d
Denys <glushychden.10@gmail.com>
2026-03-18 19:53:16 +0200

    Solve exercise.

--- dwdwd.py
+++ dwdwd.py
@@ -1,12 +1,18 @@
-nums = [3,0,1]
+n = 2
 
 
 class Solution:
-    def missing_number(self, nums: list[int]) -> int:
-        solve_all_numbers = (len(nums) * (len(nums) + 1)) / 2
-        solve_nums_numbers = sum(nums)
-        return int(solve_all_numbers - solve_nums_numbers)
+    """
+    Main idea would be like find iteration as n - 2 and after iteration + last summary of variants past iteration,
+    but I really don`t know how realize this version in code. But Gemini helps me and give idea about Fibonacci numbers
+    and his formula to solve this exercise.
+    """
+    def climb_stairs(self, n: int) -> int: # Use Fibonacci formula for solve this exercise
+        t = n - 2
+        if 1 <= n <= 45:
+            summary_of_variants = (t**2 + t + 4) / 2
+            return int(summary_of_variants)
 
 
 solution = Solution()
-print(solution.missing_number(nums))
+print(solution.climb_stairs(n))
