1013a032e9d37602bcd58b19f37ae8e9040479c1
Denys <glushychden.10@gmail.com>
2026-03-20 20:20:40 +0200

    Tried to solve exercise, but now need more time and some methods for solving this exercise.

--- dwdwd.py
+++ dwdwd.py
@@ -1,17 +1,30 @@
-import re
-
-s = "A man, a plan, a canal: Panama"
+s = "abccccdd"
 
 
 class Solution:
-    def is_palindrome(self, s: str) -> bool:
-        if 1 <= len(s) <= 2*10**5:
-            clear_str = re.sub(r'[^a-zA-Z0-9\s]', '', s).lower().replace(" ", "")
-            if clear_str == "".join(reversed(clear_str)):
-                return True
-            return False
-        return True
+    def longest_palindrome(self, s: str) -> int:
+        single_letter = False
+        result = []
+        count_letters_list = []
+        for element in s:
+            result.append(element)
+        for k in range(len(result)):
+            count_letters = result.count(result[k])
+            # print(count_letters)
+            if count_letters == 1:
+                if not single_letter:
+                    count_letters_list.append(result[k])
+                    single_letter = True
+            else:
+                count_letters_list.append(result[k])
+            # for v in range(k + 1, len(result)):
+                # print(result.count(result[k]))
+                # print(f"Haven`t any similar letter")
+
+
+        print(result)
+        print(count_letters_list)
 
 
 solution = Solution()
-print(solution.is_palindrome(s))
+print(solution.longest_palindrome(s))
