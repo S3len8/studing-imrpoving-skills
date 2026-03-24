9c72caf1bc61cd8c092590dc6acabd689c67bbc5
Denys <glushychden.10@gmail.com>
2026-03-24 12:36:16 +0200

    Solve exercise

--- dwdwd.py
+++ dwdwd.py
@@ -1,23 +1,16 @@
-s = "prog*ram**mer"
+nums = [2,14,18,22,22]
 
 
 class Solution:
-    def removeStars(self, s: str) -> str:
-        stack = []
-
-        for char in s:
-            if 1 <= len(s) <= 10**5:
-                if char == '*':
-                    # Если встретили звезду, удаляем последнюю добавленную букву
-                    if stack:
-                        stack.pop()
-                else:
-                    # Если это буква, кладем её в стек
-                    stack.append(char)
-
-        # Собираем список обратно в строку
-        return "".join(stack)
+    def contains_duplicate(self, nums: list[int]) -> bool:
+        if 1 <= len(nums) <= 10**5:
+            for element in range(0, len(nums)):
+                if -10**9 <= nums[element] <= 10**9:
+                    for element2 in range(element + 1, len(nums)):
+                        if nums[element] == nums[element2]:
+                            return True
+            return False
 
 
 solution = Solution()
-print(solution.removeStars(s))
+print(solution.contains_duplicate(nums))
