b3690748ce7552d08b3190c487819a7f3ff2b460
Denys <glushychden.10@gmail.com>
2026-03-26 19:32:15 +0200

    Done new lesson FastAPI and solve new exercise

--- dwdwd.py
+++ dwdwd.py
@@ -1,16 +1,27 @@
-nums = [2,14,18,22,22]
+nums = [0,1,0,3,12]
 
 
 class Solution:
-    def contains_duplicate(self, nums: list[int]) -> bool:
-        if 1 <= len(nums) <= 10**5:
-            for element in range(0, len(nums)):
-                if -10**9 <= nums[element] <= 10**9:
-                    for element2 in range(element + 1, len(nums)):
-                        if nums[element] == nums[element2]:
-                            return True
-            return False
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Возврат значения не требуется, все изменения выполняются на месте.
+        """
+        if 1 <= len(nums) <= 10**4:
+            for num in nums:
+                if num == 0 and -2**31 <= nums[num] <= 2**31 - 1:
+                    nums.remove(0)
+                    nums.append(0)
+
+        print(nums)
 
 
 solution = Solution()
-print(solution.contains_duplicate(nums))
+print(solution.moveZeroes(nums))
+
+# count_nums = nums.count(0)
+# i = 0
+# while i < count_nums:
+#     print(i)
+#     nums.pop(0)
+#     nums.append(0)
+#     i += 1
