e4ac3bbfb9a68700c5f1258f0358bc8cfbc194bf
Denys <glushychden.10@gmail.com>
2026-03-31 18:35:50 +0300

    Study new lesson FastAPI

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 import uvicorn
 from src.api import main_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(main_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:63342/"]
+)
+
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", reload=False)
+    uvicorn.run("src.main:app", port=8088, reload=True)
