de1d4e5fed5e9e9e5232b24eed9f0966ee5317c4
Denys <glushychden.10@gmail.com>
2026-04-01 13:22:59 +0300

    Study new lesson FastAPI

--- src/api/books.py
+++ src/api/books.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel, Field
 from sqlalchemy import select
 
 from src.api.dependencies import SessionDep
@@ -28,8 +31,16 @@ async def add_book(data: BookAddSchema, session: SessionDep):
     return {"ok": True}
 
 
+class PaginationParams(BaseModel):
+    limit: int = Field(5, ge=0, le=100, description="Кількість елементів для сторінки")
+    offset: int = Field(0, ge=0, description="Зміщення для пагінації")
+
+
+PaginationDep = Annotated[PaginationParams, Depends(PaginationParams)]
+
+
 @router.get("/books")
-async def get_books(session: SessionDep):
-    query = select(BookModel)
+async def get_books(session: SessionDep, pagination: PaginationDep) -> list[BookAddSchema]:
+    query = select(BookModel).limit(pagination.limit).offset(pagination.offset)
     result = await session.execute(query)
-    return result.scalars().all()
+    return result.scalars().all() 
