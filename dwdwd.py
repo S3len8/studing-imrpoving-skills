b26d4a1c0ce804379496bfc5b6e1f939f4cea4db
Denys <glushychden.10@gmail.com>
2026-04-06 20:55:40 +0300

    Solve new exercise with decorators

--- dwdwd.py
+++ dwdwd.py
@@ -1,131 +1,37 @@
-# def deco():
-#     ...
-#
-#
-# @deco # Перший варіант як можна застосувати декоратор синтаксичний цукор
-# def my_func():
-#     return 123
-#
-#
-# my_func = deco(my_func) # Другий варіант як можна створити декоратор
-# from collections.abc import Callable
-#
-#
-# def empty_deco(func: Callable):
-#     def wrapper():
-#         res = func()
-#         return res
-#     return wrapper
-#
-# @empty_deco
-# def my_func():
-#     return 123
-#
-# print(my_func())
-
-# from collections.abc import Callable
-# import time
-#
-# def timer_deco(func: Callable):
-#     def wrapper():
-#         start = time.perf_counter()
-#         res = func()
-#         end = time.perf_counter()
-#         print(f"Виконання зайняло: {end-start}")
-#         return res
-#     return wrapper
-#
-# @timer_deco
-# def my_func():
-#     time.sleep(1)
-#     return 123
-#
-# print(my_func())
-
-
-# from collections.abc import Callable
-# import time
-#
-# def param_timer_deco(func: Callable):
-#     def wrapper(*args, **kwargs):
-#         start = time.perf_counter()
-#         res = func(*args, **kwargs)
-#         end = time.perf_counter()
-#         print(f"Виконання зайняло: {end-start}")
-#         return res
-#     return wrapper
-#
-# @param_timer_deco
-# def my_func(sleep_time: int):
-#     time.sleep(sleep_time)
-#     return 123
-#
-# print(my_func(3))
-
-
-# from collections.abc import Callable
-# import time
-# from functools import wraps
-#
-# def limit_calls(limit: int):
-#     def wrapper(func: Callable):
-#         @wraps(func)
-#         def inner(*args, **kwargs):
-#             """Лол другий докстрінг тримай"""
-#             nonlocal limit
-#             if limit == 0:
-#                 print("Неможна користуватися функцією")
-#                 return
-#
-#             res = func(*args, **kwargs)
-#             limit -= 1
-#             return res
-#         return inner
-#     return wrapper
-#
-#
-# @limit_calls(2)
-# def my_func(sleep_time: int):
-#     """Дуже важливий докстірнг не загуби"""
-#     time.sleep(sleep_time)
-#     return 123
-#
-# print(my_func.__doc__)
-# print(my_func.__name__)
-
-
-import time
-# from functools import wraps
-# from typing import Coroutine
-# import asyncio
-#
-#
-# def deco(coroutine: Coroutine):
-#     @wraps(coroutine)
-#     async def wrapper(*args, **kwargs):
-#         res = await coroutine(*args, **kwargs)
-#         return res
-#     return wrapper
-#
-#
-# @deco
-# async def async_my_func():
-#     """Дуже важливий докстірнг не загуби"""
-#     await asyncio.sleep(0.5)
-#     return 123
-#
-#
-# print(asyncio.run(async_my_func()))
-
-
 from typing import Callable
 from functools import wraps
 
-def log_func(func: Callable):
+def add_tag(tag: str):
+    def wrapper(func: Callable):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            res = func(*args, **kwargs)
+            return f"<{tag}>{res}</{tag}>"
+        return inner
+    return wrapper
+
+def add_div(func: Callable):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f"func: {func.__name__}, args: {args}, kwargs: {kwargs}")
         res = func(*args, **kwargs)
-        return res
-    return wrapper 
+        return f"<div>{res}</div>"
+    return wrapper
 
+@add_tag("h2")
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Anton"))  # <h2>Hello, Anton!</h2>
+
+@add_div
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Grisha"))  # <div>Hello, Grisha!</div>
+
+@add_tag("h2")
+@add_div
+def greeting(name: str) -> str:
+    return f"Hello, {name}!"
+
+print(greeting("Maksim"))  # <h2><div>Hello, Maksim!</div></h2>
