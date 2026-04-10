352d53f05219ff2c083e72c244d9aa357e0194c6
Denys <glushychden.10@gmail.com>
2026-04-10 17:08:41 +0300

    Learn new lesson about Classes

--- dwdwd.py
+++ dwdwd.py
@@ -1,33 +1,22 @@
-import math
-piles = [3, 6, 7, 11]
-h = 8
+class Home():
+    default_wall_material = "Кирпич"
+
+    def __init__(self, color, floors, sec_code):
+        self.color = color # Public attribute
+        self._floors = floors # Secure attribute
+        self.__sec_code = sec_code # Private attribute
+
+    def describe(self):
+        print(f"House: {self.color}, Floors: {self._floors}, Sec Code: {self.__sec_code}")
+
+    def repaint(self, new_color):
+        self.color = new_color
+        print(f"Paint house: {self.color}")
 
 
-class Solution:
-    def min_eating_speed(self, piles: list[int], h: int) -> int:
-        sum_banana = 0
-        for banana in piles:
-            sum_banana += banana
+h1 = Home("White", 15, 893123)
+# h2 = Home("Orange", 11, 1231311)
 
-        if len(piles) < h:
-            nim_time_eating = sum_banana / h
-            return math.ceil(nim_time_eating)
-
-        if len(piles) == h:
-            nim_time_eating = max(piles)
-            return math.ceil(nim_time_eating)
-
-        # if len(piles) < h:
-        #     half_max_one = max(piles) / 2
-        #     hals_max_two = max(piles) / 2
-        #     for i in range(len(piles)):
-        #         if  piles[i] == max(piles):
-        #             piles[i] = half_max_one
-        #             piles.append(hals_max_two)
-        #
-        #     nim_time_eating = max(piles)
-        #     return math.ceil(nim_time_eating)
-
-
-solution = Solution()
-print(solution.min_eating_speed(piles, h))
+print(h1.color)
+print(h1._floors)
+print(h1.__sec_code)
